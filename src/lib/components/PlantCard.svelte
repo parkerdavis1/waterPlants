e19ed5228c364eb9421b76b1033dc63eb38cbf11
Parker <parkied@gmail.com>
2024-06-28 09:32:00 -0700

    Initial commit

---
+++ src/lib/components/PlantCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	interface Plant {
+		name: string;
+		description: string;
+	}
+	let { plant }: { plant: Plant } = $props();
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root class="m-4 w-[350px]">
+	<Card.Header>
+		<Card.Title>{plant.name}</Card.Title>
+		<Card.Description>{plant.description}</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<p>Card content...!</p>
+	</Card.Content>
+	<Card.Footer>
+		<Button variant="default">Water</Button>
+	</Card.Footer>
+</Card.Root>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import PlantCard from '$lib/components/PlantCard.svelte';
+
+	let plants = [
+		{
+			name: 'Mesquite',
+			description: 'A nice plant'
+		},
+		{
+			name: 'Palo Verde',
+			description: 'A great plant'
+		},
+		{
+			name: 'Mesquite',
+			description: 'A nice plant'
+		},
+		{
+			name: 'Palo Verde',
+			description: 'A great plant'
+		}
+	];
+</script>
+
+<main class="container mx-auto my-8">
+	<h1 class="text-2xl font-bold">🪴 Happy Plants</h1>
+	<div class="grid grid-cols-3">
+		{#each plants as plant}
+			<PlantCard {plant} />
+		{/each}
+	</div>
+</main>
