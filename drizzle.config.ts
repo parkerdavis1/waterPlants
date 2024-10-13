ac59ef5274ced11080d22be41f0ac0b5de4c46c5
Parker <parkied@gmail.com>
2024-10-13 16:44:35 -0700

    fix drizzle config with new version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'drizzle-kit';
-import env from './src/lib/env';
+import { defineConfig } from 'drizzle-kit'
+import env from './src/lib/env'
 
 export default defineConfig({
 	schema: './src/db/schema.ts',
 	out: './src/db/migrations',
-	driver: 'turso',
-	dialect: 'sqlite',
+	dialect: 'turso',
 	dbCredentials: {
 		url: env.TURSO_DATABASE_URL,
-		authToken: env.TURSO_AUTH_TOKEN
-	}
-});
+		authToken: env.TURSO_AUTH_TOKEN,
+	},
+})
