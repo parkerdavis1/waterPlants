13d28d23449ed801d1dba4aed89b2e62d050e143
Parker <parkied@gmail.com>
2025-07-19 16:59:39 -0700

    Update Docker, dependencies, and add docker-compose
    Upgraded Node and pnpm versions in Dockerfile, added docker-compose.yml for container orchestration, and updated several dependencies and devDependencies in package.json. Added pnpm-workspace.yaml and created a backup of the previous Dockerfile. Minor changes to Svelte config and database index to support updated environment.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,44 +1,50 @@
-import { drizzle } from 'drizzle-orm/libsql'
-import { createClient } from '@libsql/client'
-import env from '$lib/env'
-import * as schema from 'src/db/schema'
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+import env from "$lib/env";
+import * as schema from "src/db/schema";
 
-let client
+let client;
 if (env.LOCAL_DEV) {
 	client = createClient({
-		url: 'file:src/db/local.db',
-	})
-} else if (env.LOCAL_SEED) {
-	client = createClient({
-		url: env.TURSO_DATABASE_URL,
-		authToken: env.TURSO_AUTH_TOKEN,
-	})
+		url: "file:src/db/local.db",
+	});
 } else {
 	client = createClient({
-		url: env.TURSO_DATABASE_URL,
-		authToken: env.TURSO_AUTH_TOKEN,
-	})
-	
-	// Embedded  replicas are causing issues. Never felt fast anyways, so simplifying by just using Turso
-
-	// client = createClient({
-	// 	url: 'file:/app/data/local.db',
-	// 	syncUrl: env.TURSO_DATABASE_URL,
-	// 	authToken: env.TURSO_AUTH_TOKEN,
-	// 	syncInterval: 60,
-	// })
-
-	// console.log('Syncing database...')
-	// await client.sync()
-	// console.log('Database synced.')
+		url: "file:/data/happyplants.db",
+	});
 }
+
+// else if (env.LOCAL_SEED) {
+// 	client = createClient({
+// 		url: env.TURSO_DATABASE_URL,
+// 		authToken: env.TURSO_AUTH_TOKEN,
+// 	})
+// } else {
+// 	client = createClient({
+// 		url: env.TURSO_DATABASE_URL,
+// 		authToken: env.TURSO_AUTH_TOKEN,
+// 	})
+
+// 	// Embedded  replicas are causing issues. Never felt fast anyways, so simplifying by just using Turso
+
+// 	// client = createClient({
+// 	// 	url: 'file:/app/data/local.db',
+// 	// 	syncUrl: env.TURSO_DATABASE_URL,
+// 	// 	authToken: env.TURSO_AUTH_TOKEN,
+// 	// 	syncInterval: 60,
+// 	// })
+
+// 	// console.log('Syncing database...')
+// 	// await client.sync()
+// 	// console.log('Database synced.')
+// }
 
 const db = drizzle(client, {
 	logger: true,
 	schema,
-})
+});
 
-export type db = typeof db
-export default db
-export type Client = typeof client
-export { client }
+export type db = typeof db;
+export default db;
+export type Client = typeof client;
+export { client };
