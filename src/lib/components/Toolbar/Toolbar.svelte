aad1911dd0426606ba4633c4b0853df4ca320027
Parker <parkied@gmail.com>
2025-11-08 17:50:32 -0700

    Toolbar tweaks, sticky + icons

--- src/lib/components/Toolbar/Toolbar.svelte
+++ src/lib/components/Toolbar/Toolbar.svelte
@@ -6,23 +6,25 @@
 	import Label from '../ui/label/label.svelte'
 	import { scale } from 'svelte/transition'
 	import { waterPlantsView, waterMultipleOpen } from 'src/lib/stores/viewStore.svelte'
+	import { CirclePlus, Droplets, Plus, Sprout } from 'lucide-svelte'
 
 	const { data, waterDisabled } = $props()
 
 	// const checked = $state(waterPlantsView)
 </script>
 
-<div class="my-4 flex flex-wrap justify-between gap-2">
+<div class="sticky top-0 z-50 my-4 flex flex-wrap justify-between gap-2 bg-background py-2">
 	<div class="flex flex-wrap items-center gap-2">
-		<a href="/new/plant"><Button>Add New Plant</Button></a>
+		<a href="/new/plant"><Button><Sprout />New</Button></a>
 		{#if !waterDisabled}
 			<div transition:scale>
-				<Button onclick={() => (waterMultipleOpen.value = true)}>Water Plants</Button>
+				<Button onclick={() => (waterMultipleOpen.value = true)}><Droplets />Water</Button>
 			</div>
 		{/if}
 	</div>
 	<div class="flex items-center gap-2">
 		<Switch id="showWaterPlants" bind:checked={waterPlantsView.value} />
-		<Label for="showWaterPlants">Only Show Thirsty Plants</Label>
+		<Label for="showWaterPlants" class="hidden sm:inline">Only Show Thirsty Plants</Label>
+		<Label for="showWaterPlants" class="sm:hidden">Thirsty Only</Label>
 	</div>
 </div>
