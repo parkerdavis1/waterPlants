48fd48427a3859e1fcbc4e0a3f237bb446c3144e
Parker <parkied@gmail.com>
2026-08-01 12:05:55 -0700

    Fix migration

--- src/db/migrations/0004_curly_piledriver.sql
+++ src/db/migrations/0004_curly_piledriver.sql
@@ -10,12 +10,4 @@ CREATE TABLE `push_subscription` (
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `push_subscription_endpoint_unique` ON `push_subscription` (`endpoint`);--> statement-breakpoint
-DROP INDEX "push_subscription_endpoint_unique";--> statement-breakpoint
-ALTER TABLE `house` ALTER COLUMN "created_at" TO "created_at" integer NOT NULL DEFAULT (unixepoch('subsec') * 1000);--> statement-breakpoint
-ALTER TABLE `plant` ALTER COLUMN "created_at" TO "created_at" integer NOT NULL DEFAULT (unixepoch('subsec') * 1000);--> statement-breakpoint
-ALTER TABLE `plant` DROP COLUMN `waitUntil`;--> statement-breakpoint
-ALTER TABLE `room` ALTER COLUMN "created_at" TO "created_at" integer NOT NULL DEFAULT (unixepoch('subsec') * 1000);--> statement-breakpoint
-ALTER TABLE `user` ALTER COLUMN "created_at" TO "created_at" integer NOT NULL DEFAULT (unixepoch('subsec') * 1000);--> statement-breakpoint
-ALTER TABLE `watering_event` ALTER COLUMN "timestamp" TO "timestamp" integer NOT NULL DEFAULT (unixepoch('subsec') * 1000);--> statement-breakpoint
-ALTER TABLE `watering_event` ADD `waitUntil` integer;--> statement-breakpoint
 ALTER TABLE `watering_event` ADD `notified_at` integer;
