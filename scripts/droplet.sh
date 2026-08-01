1d2d98aceabeaa5d9302f4c823170981f0f39728
Parker <parkied@gmail.com>
2026-08-01 11:56:54 -0700

    Fix deploy to use main branch

--- scripts/droplet.sh
+++ scripts/droplet.sh
@@ -12,8 +12,12 @@ rsync -a .env.prod droplet:/root/waterPlants/
 ssh droplet '
     cd /root/waterPlants
 
+    echo "🔎 Checking out main..."
+    git fetch origin
+    git checkout main
+
     echo "⬇️ Pulling from github..."
-    git pull
+    git pull origin main
 
     echo "🏗️ Building image..."
     docker compose -f /root/waterPlants/docker-compose.yml build
